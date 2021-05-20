ac5d86790a84f8355cd74f4b17bee0b2c3d118bf
YuliaR <ula.rafikova@aisa.ru>
2021-05-20 18:19:58 +0300

    Решение задачи Компонент CounterButton

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,6 +1,16 @@
 export default {
   name: 'CounterButton',
-
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+      increment(value) {
+        this.$emit('increment', value+1);
+      }
+  },
   // Компонент должен иметь входной параметр
 
   // Компонент должен иметь модель
@@ -8,5 +18,5 @@ export default {
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" @click="increment(Number($event.target.value))">{{ count }}</button>',
 };

f309f5cbd451e1b5c86b6616db632b9405939848
YuliaR <ula.rafikova@aisa.ru>
2021-05-21 17:24:29 +0300

    не доделанная задача MeetupCover

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -1,9 +1,23 @@
 const MeetupCover = {
   name: 'MeetupCover',
-
+  props: {
+    title: {
+      type: String,
+      required: false
+    },
+    link: {
+      type: String,
+      required: false
+    }
+  },
+  computed: {
+    nImage(){
+      return this.link===null ? "--bg-url: url('https://course-vue.javascript.ru/api/images/2')" : "--bg-url: url('"+this.link+"')"      
+    }
+  },
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="nImage">
+        <h1 class="meetup-cover__title">{{title}}</h1>
     </div>`,
 };
 

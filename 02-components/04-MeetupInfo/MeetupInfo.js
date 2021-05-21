5f822df734258b43713f922e3559467344deb0cd
YuliaR <ula.rafikova@aisa.ru>
2021-05-21 15:11:21 +0300

    Решение задачи MeetupInfo

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -1,19 +1,33 @@
 export default {
   name: 'MeetupInfo',
+  props: {
+    place: {
+      type: String,
+      required: true
+    },
+    organizer: {
+      type: String,
+      required: true
+    },    
+    date: {
+      type: Date,
+      required: true
+    },
 
+  },
   template: `
     <ul class="info-list">
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{organizer}}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{place}}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time datetime="2020-01-01">{{date}}</time>
       </li>
     </ul>`,
 };

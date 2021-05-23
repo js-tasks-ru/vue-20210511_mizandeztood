7c98f0d23a38787a1f7a1f60cb58bc13b3a48c03
YuliaR <ula.rafikova@aisa.ru>
2021-05-23 21:25:42 +0300

    Решение задачи MeetupAgenda

--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,14 +1,19 @@
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 const MeetupAgenda = {
   name: 'MeetupAgenda',
-
+  components: {
+    MeetupAgendaItem,
+  },
+  props: {
+    agenda: {
+      type: Array,
+      requied: true
+    }
+  },
   template: `
-    <div class="meetup-agenda">
-      <div class="meetup-agenda__item"></div>
-      <div class="meetup-agenda__item"></div>
-      <!-- ... -->
-      <div class="meetup-agenda__item"></div>
+    <div class="meetup-agenda">     
+      <meetup-agenda-item v-for="(a, index) in agenda" :agendaItem="agenda[index]"></meetup-agenda-item>      
     </div>`,
 };
 
--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -1,22 +1,37 @@
-// import { agendaItemIcons, agendaItemDefaultTitles } from './data.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from './data.js';
 
 const MeetupAgendaItem = {
   name: 'MeetupAgendaItem',
 
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    itemTitle(){
+      return this.agendaItem.title===null ? agendaItemDefaultTitles[this.agendaItem.type] : this.agendaItem.title
+    },
+    itemIcon(){
+      return agendaItemIcons[this.agendaItem.type]
+    }
+  },
+
   template: `
     <div class="meetup-agenda__item">
       <div class="meetup-agenda__item-col">
-        <img class="icon" alt="icon" src="/assets/icons/icon-cal-sm.svg" />
+        <img class="icon" alt="icon" :src="'/assets/icons/icon-'+itemIcon+'.svg'" />
       </div>
-      <div class="meetup-agenda__item-col">00:00 - 00:00</div>
+      <div class="meetup-agenda__item-col">{{agendaItem.startsAt}} - {{agendaItem.endsAt}}</div>
       <div class="meetup-agenda__item-col">
-        <h5 class="meetup-agenda__title">Заголовок</h5>
-        <p>
-          <span>Докладчик</span>
+        <h5 class="meetup-agenda__title">{{itemTitle}}</h5>
+        <p v-if="agendaItem.type === 'talk'">
+          <span>{{agendaItem.speaker}}</span>
           <span class="meetup-agenda__dot"></span>
-          <span class="meetup-agenda__lang">ru</span>
+          <span class="meetup-agenda__lang">{{agendaItem.language}}</span>
         </p>
-        <p>Описание</p>
+        <p v-if="agendaItem.description">{{agendaItem.description}}</p>
       </div>
     </div>`,
 };

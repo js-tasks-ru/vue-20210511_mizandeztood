26225a1abac4b36e824806f4eb0029554f9548a5
YuliaR <ula.rafikova@aisa.ru>
2021-05-24 16:37:43 +0300

    Решение задачи MeetupPageComponent

--- 02-components/07-MeetupPageComponent/MeetupPage.js
+++ 02-components/07-MeetupPageComponent/MeetupPage.js
@@ -1,10 +1,25 @@
-// import MeetupView from './MeetupView.js';
-// import { MEETUP_ID, fetchMeetup } from './data.js';
+import MeetupView from './MeetupView.js';
+import { MEETUP_ID, fetchMeetup } from './data.js';
 
 const MeetupPage = {
   name: 'MeetupPage',
-
-  template: `<div></div>`,
+  components: {
+    MeetupView,
+    MEETUP_ID,
+    fetchMeetup
+  },
+  data() {
+    return {
+      rawMeetup: null
+    };
+  },
+  mounted() {    
+    fetchMeetup(MEETUP_ID).then((meetup) => {     
+      this.rawMeetup = meetup;
+    });
+  },
+  //template: `<div></div>`,
+  template: `<meetup-view v-if="rawMeetup" :meetup="rawMeetup"></meetup-view>`
 };
 
 export default MeetupPage;
